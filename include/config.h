6d181fc4d1feec57a046e3a82a262d4777764ccf
karl <larku@larku.com>
2025-06-09 21:33:50 +0930

    Added private config

--- include/config.h
+++ include/config.h
@@ -1,16 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define WIFI_SSID "house"
-#define WIFI_PASSWORD "Can I please play?"
-
-#define MQTT_SERVER "192.168.50.11"
-#define MQTT_PORT 1883
-#define MQTT_USER "rinnai"
-#define MQTT_PASSWORD "rinnai"
-
-#define DEVICE_NAME "ac-zone-controller"
-#define BASE_TOPIC "ac-zone-controller"
+#include "config-private.h"
 
 #define NUM_ZONES 13
 #define MASTER_RELAY_INDEX 15
